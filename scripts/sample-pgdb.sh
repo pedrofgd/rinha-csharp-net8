184ac7e0edee68d0d7e41295e7c091d60c3b550d
Pedro Dias <mnopedrodias@gmail.com>
2023-09-23 14:32:55 -0300

    postgresql.conf para o banco de testes

--- scripts/sample-pgdb.sh
+++ scripts/sample-pgdb.sh
@@ -6,5 +6,8 @@ docker run \
     -e POSTGRES_PASSWORD=123 \
     -e POSTGRES_DB=rinha \
     -p 5432:5432 \
+    -v ./postgresql.conf:/etc/postgresql.conf:ro \
     -v ./init.sql:/docker-entrypoint-initdb.d/init.sql:ro \
-    postgres
+    --memory=2000m \
+    --cpus=0.5 \
+    postgres postgres -c config_file=/etc/postgresql.conf
